4ea2b0b406b681ad59d59c7bc1204ccc956a5fcd
JanithSandaru <janithsandaru999@gmail.com>
2022-06-12 20:11:36 +0530

    added configurations

---
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+
+    @GetMapping
+    public String getAllCustomers(){
+        return "Hello Customer";
+    }
+}
